9e805d389339a1fc8f5eb22edc81e694923d45c6
shreyvishal <shreya.vishal@eminds.ai>
2024-03-27 19:47:41 +0530

    Resolved Issue #3 and created a function to return message from the json response feeded to it

--- services/AI.js
+++ services/AI.js
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from 'fs'
 import OpenAI from 'openai'
 import logger from '../utils/logger.js'
 import yaml from 'js-yaml'
@@ -10,78 +10,121 @@ const openai = new OpenAI({
 const config = JSON.parse(readFileSync('./config/openai.json'))
 
 class AI {
-    
     constructor() {
-        this.context = [];
+        this.context = []
     }
-    
+
     /**
-    * Function to retuen a beckn action from given text. 
-    * It should return a chat completion response if no action is found.
-    * @param {*} text 
-    * @returns 
-    */
-    async get_beckn_action_from_text(text){
+     * Function to retuen a beckn action from given text.
+     * It should return a chat completion response if no action is found.
+     * @param {*} text
+     * @returns
+     */
+
+    async get_message_from_beckn_response(json_response) {
         const openai_messages = [
-            { role: 'system', content: `Your job is to analyse the text input given by user and identify if that is an action based on given set of actions. The supported actions with their descriptions are : ${JSON.stringify(config.SUPPORTED_ACTIONS)}.` }, 
-            { role: 'system', content: `You must return a json in the following format {'action':'SOME_ACTION_OR_NULL', 'response': 'Should be reponse based on the query.'}` },
-            { role: 'system', content: `If the instruction is an action, the action key should be set under 'action' otehrwise action should be null and response should contain completion for the given text.` }, 
-            { role: 'system', content: `A typical order flow should be search > select > init > confirm.`},
-            { role: 'system', content: `If you are asked to prepare an itinery or plan a trip, always ask for user preferences such as accommodation types, journey details, dietary preferences, things of interest, journey dates, journey destination, number of members, special requests.` }, 
-            { role: 'user', content: text}
+            {
+                role: 'system',
+                content: `You must validate this JSON object structure ${JSON.stringify(json_response)}. If JSON object is empty return a response in JSON object format {success:false, message:Empty JSON}. And if the JSON is invalid you should create an error message and set that in the message key of JSON object {success:false}. If its valid then create a meaningful text message containing all the information present in this json ${JSON.stringify(json_response)} based on the action description in the responses array of the json and return the message `,
+            },
         ]
-        
         let response = {
             action: null,
-            response: null
+            response: null,
         }
-        try{
+        try {
             const completion = await openai.chat.completions.create({
                 messages: openai_messages,
                 model: process.env.OPENAI_MODEL_ID,
                 temperature: 0,
-                response_format: { type: 'json_object' }
+                response_format: { type: 'json_object' },
             })
-            response = JSON.parse(completion.choices[0].message.content);
+            response = JSON.parse(completion.choices[0].message.content)
+            if (!response.success) {
+                throw new Error(response.message || 'Invalid JSON')
+            }
+        } catch (e) {
+            logger.error(e)
         }
-        catch(e){
-            logger.error(e);
-        }
-        return response;
+        return response
     }
-    
-    async get_beckn_request_from_text(instruction, context=[]){
+    async get_beckn_action_from_text(text) {
+        const openai_messages = [
+            {
+                role: 'system',
+                content: `Your job is to analyse the text input given by user and identify if that is an action based on given set of actions. The supported actions with their descriptions are : ${JSON.stringify(config.SUPPORTED_ACTIONS)}.`,
+            },
+            {
+                role: 'system',
+                content: `You must return a json in the following format {'action':'SOME_ACTION_OR_NULL', 'response': 'Should be reponse based on the query.'}`,
+            },
+            {
+                role: 'system',
+                content: `If the instruction is an action, the action key should be set under 'action' otehrwise action should be null and response should contain completion for the given text.`,
+            },
+            {
+                role: 'system',
+                content: `If you are asked to prepare an itinery or plan a trip, always ask for user preferences such as accommodation types, journey details, dietary preferences, things of interest, journey dates, journey destination, number of members, special requests.`,
+            },
+            { role: 'user', content: text },
+        ]
+
+        let response = {
+            action: null,
+            response: null,
+        }
+        try {
+            const completion = await openai.chat.completions.create({
+                messages: openai_messages,
+                model: process.env.OPENAI_MODEL_ID,
+                temperature: 0,
+                response_format: { type: 'json_object' },
+            })
+            response = JSON.parse(completion.choices[0].message.content)
+        } catch (e) {
+            logger.error(e)
+        }
+        return response
+    }
+
+    async get_beckn_request_from_text(instruction, context = []) {
         let action_response = {
             status: false,
             data: null,
-            message: null
+            message: null,
         }
-        
-        
+
         // Preparse presets
         let presets = {
             ...config.PRESETS,
-            domain : "Any of these : uei:charging",
-            message_id : uuidv4(),
+            domain: 'Any of these : uei:charging',
+            message_id: uuidv4(),
             transaction_id: uuidv4(),
-            action :`Any of these : ${JSON.stringify(config.SUPPORTED_DOMAINS.map(item => item.key))}`
+            action: `Any of these : ${JSON.stringify(config.SUPPORTED_DOMAINS.map((item) => item.key))}`,
         }
-        
+
         // get the right/compressed schema
-        const schema_response = await this._get_schema_by_instruction(instruction)
-        const schema = schema_response.data;
+        const schema_response =
+            await this._get_schema_by_instruction(instruction)
+        const schema = schema_response.data
 
         // If its a valid action
-        if(schema_response.status){
+        if (schema_response.status) {
             let openai_messages = [
-                { "role": "system", "content": `Schema definition: ${JSON.stringify(schema)}` },
+                {
+                    role: 'system',
+                    content: `Schema definition: ${JSON.stringify(schema)}`,
+                },
                 ...config.SCHEMA_TRANSLATION_CONTEXT,
-                {"role": "system", "content": `Use the following presets to fill the context : ${JSON.stringify(presets)}`},
+                {
+                    role: 'system',
+                    content: `Use the following presets to fill the context : ${JSON.stringify(presets)}`,
+                },
                 ...context,
-                { "role": "user", "content": instruction }
+                { role: 'user', content: instruction },
             ]
-            
-            try{
+
+            try {
                 const completion = await openai.chat.completions.create({
                     messages: openai_messages,
                     model: process.env.OPENAI_MODEL_ID,
@@ -90,45 +133,57 @@ class AI {
                 })
                 const jsonString = completion.choices[0].message.content.trim()
                 logger.info(jsonString)
-                logger.info(`\u001b[1;34m ${JSON.stringify(completion.usage)}\u001b[0m`)
-                
+                logger.info(
+                    `\u001b[1;34m ${JSON.stringify(completion.usage)}\u001b[0m`
+                )
+
                 const response = JSON.parse(jsonString)
                 response.url = `${config.PRESETS.base_url}/${response.body.context.action}`
-                action_response = {...action_response, status: true, data: response}
-            }
-            catch(e){
-                logger.error(e);
-                action_response = {...action_response, message: e.message}
-            }
-        }
-        else{
-            action_response = {...action_response, message: schema_response.message}
-        }
-        
-        
-        return action_response;
-    }
-    
-    async compress_search_results(search_res){
-
-        const desired_output = {
-            "providers": [
-                {
-                    "id": "some_provider_id",
-                    "name": "some_provider_name",
-                    "items": [
-                        {
-                            "id": "some_item_id",
-                            "name": "some_item_name"
-                        }
-                    ]
+                action_response = {
+                    ...action_response,
+                    status: true,
+                    data: response,
                 }
-            ]
+            } catch (e) {
+                logger.error(e)
+                action_response = { ...action_response, message: e.message }
+            }
+        } else {
+            action_response = {
+                ...action_response,
+                message: schema_response.message,
+            }
+        }
+
+        return action_response
+    }
+
+    async compress_search_results(search_res) {
+        const desired_output = {
+            providers: [
+                {
+                    id: 'some_provider_id',
+                    name: 'some_provider_name',
+                    items: [
+                        {
+                            id: 'some_item_id',
+                            name: 'some_item_name',
+                        },
+                    ],
+                },
+            ],
         }
         let openai_messages = [
-            { "role" : "system", "content": `Your job is to complress the search results received from user into the following JSON structure : ${JSON.stringify(desired_output)}`},
-            { "role" : "system", "content": "you should not send providers that do not have items." },
-            { "role": "user", "content": JSON.stringify(search_res)}
+            {
+                role: 'system',
+                content: `Your job is to complress the search results received from user into the following JSON structure : ${JSON.stringify(desired_output)}`,
+            },
+            {
+                role: 'system',
+                content:
+                    'you should not send providers that do not have items.',
+            },
+            { role: 'user', content: JSON.stringify(search_res) },
         ]
         const completion = await openai.chat.completions.create({
             messages: openai_messages,
@@ -139,43 +194,46 @@ class AI {
         const jsonString = completion.choices[0].message.content.trim()
         logger.info(jsonString)
         logger.info(`\u001b[1;34m ${JSON.stringify(completion.usage)}\u001b[0m`)
-        
+
         const compressed = JSON.parse(jsonString)
-        return {...search_res, responses: compressed};
+        return { ...search_res, responses: compressed }
     }
-    
+
     async _get_schema_by_instruction(instruction) {
         let response = {
             status: false,
             data: null,
-            message : null
+            message: null,
         }
-        const action = await this.get_beckn_action_from_text(instruction);
-        if(action?.action){
+        const action = await this.get_beckn_action_from_text(instruction)
+        if (action?.action) {
             try {
-                const filePath = `./schemas/core_1.1.0/${action?.action}.yml`;
-                const schema = yaml.load(readFileSync(filePath, 'utf8'));
-                response = {...response, status: true, data: schema};
+                const filePath = `./schemas/core_1.1.0/${action?.action}.yml`
+                const schema = yaml.load(readFileSync(filePath, 'utf8'))
+                response = { ...response, status: true, data: schema }
             } catch (error) {
-                const defaultFilePath = './schemas/core_1.1.0.yml';
-                const defaultSchema = yaml.load(readFileSync(defaultFilePath, 'utf8'));
-                
+                const defaultFilePath = './schemas/core_1.1.0.yml'
+                const defaultSchema = yaml.load(
+                    readFileSync(defaultFilePath, 'utf8')
+                )
+
                 // Reduce schema
-                const specificSchema = JSON.stringify(defaultSchema.paths[`/${action.action}`])
+                const specificSchema = JSON.stringify(
+                    defaultSchema.paths[`/${action.action}`]
+                )
                 if (specificSchema) {
                     defaultSchema.paths = {
                         [`/${action.action}`]: specificSchema,
                     }
                 }
-                
-                response = {...response, status: true, data: defaultSchema};
+
+                response = { ...response, status: true, data: defaultSchema }
             }
+        } else {
+            response = { ...response, message: action.response }
         }
-        else{
-            response = {...response, message: action.response}
-        }
-        return response;
+        return response
     }
 }
 
-export default AI;
+export default AI
--- tests/unit/services/ai.test.js
+++ tests/unit/services/ai.test.js
@@ -1,73 +1,95 @@
 import * as chai from 'chai'
 const expect = chai.expect
 import AI from '../../../services/AI.js'
-import logger from '../../../utils/logger.js';
-import { readFileSync } from 'fs';
-const ai = new AI();
-const on_search = JSON.parse(readFileSync('./tests/data/api_responses/on_search.json'))
-const on_search_compressed = JSON.parse(readFileSync('./tests/data/api_responses/on_search_compressed.json'))
-const on_select = JSON.parse(readFileSync('./tests/data/api_responses/on_select.json'))
-const on_init = JSON.parse(readFileSync('./tests/data/api_responses/on_init.json'))
-
+import logger from '../../../utils/logger.js'
+import { readFileSync } from 'fs'
+const ai = new AI()
+const on_search = JSON.parse(
+    readFileSync('./tests/data/api_responses/on_search.json')
+)
+const on_search_compressed = JSON.parse(
+    readFileSync('./tests/data/api_responses/on_search_compressed.json')
+)
+const on_select = JSON.parse(
+    readFileSync('./tests/data/api_responses/on_select.json')
+)
+const on_init = JSON.parse(
+    readFileSync('./tests/data/api_responses/on_init.json')
+)
 
 describe('Test cases for services/ai/get_beckn_action_from_text()', () => {
-    
     it('Should test get_beckn_action_from_text() succesfully for a search intent', async () => {
-        const response = await ai.get_beckn_action_from_text("I'm looking for some ev chargers.");
-        expect(response.action).to.equal('search');
+        const response = await ai.get_beckn_action_from_text(
+            "I'm looking for some ev chargers."
+        )
+        expect(response.action).to.equal('search')
     })
-    
+
     it('Should test get_beckn_action_from_text() succesfully for a select intent', async () => {
-        const response = await ai.get_beckn_action_from_text('I want to add this to the cart.');
-        expect(response.action).to.equal('select');
+        const response = await ai.get_beckn_action_from_text(
+            'I want to add this to the cart.'
+        )
+        expect(response.action).to.equal('select')
     })
-    
+
     it('Should test get_beckn_action_from_text() fail for a general query', async () => {
-        const response = await ai.get_beckn_action_from_text('What is 2+2?');
-        expect(response.action).to.be.null;
+        const response = await ai.get_beckn_action_from_text('What is 2+2?')
+        expect(response.action).to.be.null
     })
-    
+
     it('Should test get_beckn_action_from_text() succesfully for an itinerary', async () => {
-        const response = await ai.get_beckn_action_from_text('I want to plan a trip to the mountains. Can you please help me with that?');
+        const response = await ai.get_beckn_action_from_text(
+            'I want to plan a trip to the mountains. Can you please help me with that?'
+        )
         // logging the resopmse as there is no assertion to be made here.
-        logger.info(response.response);
-        expect(response.action).to.be.null;
+        logger.info(response.response)
+        expect(response.action).to.be.null
     })
 
     it('Should test get_beckn_action_from_text() fail if called init without the billing details', async () => {
-        const response = await ai.get_beckn_action_from_text('Lets place the order');
-        expect(response.action).to.be.null;
+        const response = await ai.get_beckn_action_from_text(
+            'Lets place the order'
+        )
+        expect(response.action).to.be.null
     })
 })
 
 describe('Test cases for services/ai/compress_search_results()', () => {
     it('Should test succesful compression of search results', async () => {
-        const compressed = await ai.compress_search_results(on_search);
+        const compressed = await ai.compress_search_results(on_search)
         expect(compressed.responses.providers).to.be.an('array')
         expect(compressed.responses.providers.length).to.be.greaterThan(0)
         expect(compressed.responses.providers[0]).to.have.property('id')
         expect(compressed.responses.providers[0]).to.have.property('name')
-        expect(compressed.responses.providers[0]).to.have.property('items');
+        expect(compressed.responses.providers[0]).to.have.property('items')
     })
 })
 
 describe('Test cases for services/ai/get_beckn_request_from_text()', () => {
     it('Should test get_beckn_request_from_text() succesfully for a `search` intent', async () => {
-        const response = await ai.get_beckn_request_from_text("I'm looking for some ev chargers.");
-        expect(response.status).to.be.eq(true);
+        const response = await ai.get_beckn_request_from_text(
+            "I'm looking for some ev chargers."
+        )
+        expect(response.status).to.be.eq(true)
         expect(response.data).to.be.an('object')
         expect(response.data.method.toUpperCase()).to.be.eq('POST')
         expect(response.data.url).to.contain('search')
-        expect(response.data.body.message.intent.descriptor).to.have.property('name')
-    })    
+        expect(response.data.body.message.intent.descriptor).to.have.property(
+            'name'
+        )
+    })
     it('Should test get_beckn_request_from_text() succesfully for a `select`', async () => {
-        
         let context = [
-            {"role": "user", "content": "I'm looking for some ev chargers"},
-            {"role": "assistant", "content": JSON.stringify(on_search_compressed)}
-            
+            { role: 'user', content: "I'm looking for some ev chargers" },
+            {
+                role: 'assistant',
+                content: JSON.stringify(on_search_compressed),
+            },
         ]
-        const response = await ai.get_beckn_request_from_text("Lets select the first item", context);
+        const response = await ai.get_beckn_request_from_text(
+            'Lets select the first item',
+            context
+        )
         expect(response.data).to.be.an('object')
         expect(response.data.method.toUpperCase()).to.be.eq('POST')
         expect(response.data.url).to.contain('select')
@@ -81,12 +103,18 @@ describe('Test cases for services/ai/get_beckn_request_from_text()', () => {
 
     it('Should test get_beckn_request_from_text() succesfully for a `init`', async () => {
         let context = [
-            {"role": "user", "content": "I'm looking for some ev chargers"},
-            {"role": "assistant", "content": JSON.stringify(on_search_compressed)},
-            {"role": "user", "content": "I want to select the first item"},
-            {"role": "assistant", "content": JSON.stringify(on_select)}
+            { role: 'user', content: "I'm looking for some ev chargers" },
+            {
+                role: 'assistant',
+                content: JSON.stringify(on_search_compressed),
+            },
+            { role: 'user', content: 'I want to select the first item' },
+            { role: 'assistant', content: JSON.stringify(on_select) },
         ]
-        const response = await ai.get_beckn_request_from_text("Lets place the order. My details are : Mayur Virendra, 9986949245, mayurlibra@gmail.com", context);
+        const response = await ai.get_beckn_request_from_text(
+            'Lets place the order. My details are : Mayur Virendra, 9986949245, mayurlibra@gmail.com',
+            context
+        )
         expect(response.data).to.be.an('object')
         expect(response.data.method.toUpperCase()).to.be.eq('POST')
         expect(response.data.url).to.contain('init')
@@ -97,32 +125,54 @@ describe('Test cases for services/ai/get_beckn_request_from_text()', () => {
         expect(response.data.body.message.order).to.have.property('items')
         expect(response.data.body.message.order.items[0]).to.have.property('id')
         expect(response.data.body.message.order).to.have.property('billing')
-        expect(response.data.body.message.order.billing).to.have.property('name')
-        expect(response.data.body.message.order.billing).to.have.property('email')
-        expect(response.data.body.message.order.billing).to.have.property('phone')
-    });
+        expect(response.data.body.message.order.billing).to.have.property(
+            'name'
+        )
+        expect(response.data.body.message.order.billing).to.have.property(
+            'email'
+        )
+        expect(response.data.body.message.order.billing).to.have.property(
+            'phone'
+        )
+    })
 
     it('Should test get_beckn_request_from_text() to return with questions if billing details are not provided for  `init`', async () => {
         let context = [
-            {"role": "user", "content": "I'm looking for some ev chargers"},
-            {"role": "assistant", "content": JSON.stringify(on_search_compressed)},
-            {"role": "user", "content": "I want to select the first item"},
-            {"role": "assistant", "content": JSON.stringify(on_select)}
+            { role: 'user', content: "I'm looking for some ev chargers" },
+            {
+                role: 'assistant',
+                content: JSON.stringify(on_search_compressed),
+            },
+            { role: 'user', content: 'I want to select the first item' },
+            { role: 'assistant', content: JSON.stringify(on_select) },
         ]
-        const response = await ai.get_beckn_request_from_text("Lets place the order.", context);
-        expect(response.status).to.be.eq(false);
-    });
+        const response = await ai.get_beckn_request_from_text(
+            'Lets place the order.',
+            context
+        )
+        expect(response.status).to.be.eq(false)
+    })
 
     it('Should test get_beckn_request_from_text() succesfully for a `confirm`', async () => {
         let context = [
-            {"role": "user", "content": "I'm looking for some ev chargers"},
-            {"role": "assistant", "content": JSON.stringify(on_search_compressed)},
-            {"role": "user", "content": "I want to select the first item"},
-            {"role": "assistant", "content": JSON.stringify(on_select)},
-            {"role": "user", "content": "Lets place the order. My details are : Mayur Virendra, 9986949245, mayurlibra@gmail.com"},
-            {"role": "assistant", "content": JSON.stringify(on_init)}
+            { role: 'user', content: "I'm looking for some ev chargers" },
+            {
+                role: 'assistant',
+                content: JSON.stringify(on_search_compressed),
+            },
+            { role: 'user', content: 'I want to select the first item' },
+            { role: 'assistant', content: JSON.stringify(on_select) },
+            {
+                role: 'user',
+                content:
+                    'Lets place the order. My details are : Mayur Virendra, 9986949245, mayurlibra@gmail.com',
+            },
+            { role: 'assistant', content: JSON.stringify(on_init) },
         ]
-        const response = await ai.get_beckn_request_from_text("Lets confirm the order!", context);
+        const response = await ai.get_beckn_request_from_text(
+            'Lets confirm the order!',
+            context
+        )
         expect(response.data).to.be.an('object')
         expect(response.data.method.toUpperCase()).to.be.eq('POST')
         expect(response.data.url).to.contain('confirm')
@@ -133,8 +183,26 @@ describe('Test cases for services/ai/get_beckn_request_from_text()', () => {
         expect(response.data.body.message.order).to.have.property('items')
         expect(response.data.body.message.order.items[0]).to.have.property('id')
         expect(response.data.body.message.order).to.have.property('billing')
-        expect(response.data.body.message.order.billing).to.have.property('name')
-        expect(response.data.body.message.order.billing).to.have.property('email')
-        expect(response.data.body.message.order.billing).to.have.property('phone')
-    });
-});
+        expect(response.data.body.message.order.billing).to.have.property(
+            'name'
+        )
+        expect(response.data.body.message.order.billing).to.have.property(
+            'email'
+        )
+        expect(response.data.body.message.order.billing).to.have.property(
+            'phone'
+        )
+    })
+})
+
+describe('Test cases for services/ai/get_message_from_beckn_response()', () => {
+    it('Should test get_message_from_beckn_response() and throw response with success false for empty object', async () => {
+        const response = await ai.get_message_from_beckn_response({})
+        expect(response.success).to.equal(false)
+        expect(response.message).to.equal('Empty JSON')
+    })
+    it('Should test get_message_from_beckn_response() return some message with success true', async () => {
+        const response = await ai.get_message_from_beckn_response(on_init)
+        expect(response.success).to.equal(true)
+    })
+})
