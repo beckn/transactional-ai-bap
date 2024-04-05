6357c01332d7c8e65e21b09f4d0961849a5a8d1f
Mayur Virendra <mayurlibra@gmail.com>
2024-04-05 13:23:18 +0530

    remobved deprecated code

--- services/Actions.js
+++ services/Actions.js
@@ -55,55 +55,6 @@ class Actions {
         return responseObject
     }
     
-    async process_instruction(message, context=[]) {
-        let response = {
-            status: false,
-            formatted: 'Failed to process the instruction',
-        }
-        try {
-
-            // Get action from text message
-            this.ai.action = await this.ai.get_beckn_action_from_text(message, context);
-            if(this.ai.action?.action === 'search') context = [];
-
-            // Get becnk request from text message
-            const beckn_request = await this.ai.get_beckn_request_from_text(message, context);
-            if(!beckn_request.status){
-                response.formatted = beckn_request.message;              
-            }
-            else{
-                // Call the API
-                logger.info(`Making api call...`)
-                const call_api_response = await this.call_api(beckn_request.data.url, beckn_request.data.method, beckn_request.data.body, beckn_request.data.headers)
-                if(!call_api_response.status){
-                    response.formatted = `Failed to call the API: ${call_api_response.error}`
-                    response.data = call_api_response.data              
-                }
-                else{
-
-                    logger.info(`API call successful. Compessing search results in case of search...`)
-                    response = {
-                        status: true,
-                        raw: beckn_request.data.body.context.action==='search' ? await this.ai.compress_search_results(call_api_response.data) : call_api_response.data
-                    }
-
-                    // Format the response
-                    logger.info(`Formatting response...`);
-                    const get_text_from_json_response = await this.ai.get_text_from_json(
-                        call_api_response.data,
-                        [...context, { role: 'user', content: message }]
-                    )
-                    response.formatted = get_text_from_json_response.message
-                }                
-            }            
-        } catch (error) {
-            logger.error(`Error processing instruction: ${error.message}`)
-            response.formatted = `Failed to process the instruction: ${error.message}`
-        }
-        
-        return response;
-    }
-    
     async send_message(recipient, message) {
         try {
             const data = await client.messages.create({
