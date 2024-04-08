5af567b90049984f86653b21dea156f66ce49b03
shreyvishal <shreya.vishal@eminds.ai>
2024-04-08 22:56:33 +0530

    Fixed: lint Issue

--- services/CronService.js
+++ services/CronService.js
@@ -1,64 +1,40 @@
 import {
-    TOURISM_STRAPI_URL,
-    HOTEL_STRAPI_URL,
-    RETAIL_STRAPI_URL,
-    ENERGY_STRAPI_URL,
     DOMAINS,
     BECKN_STATUS_CALL
 } from '../utils/constants.js'
 import ActionsService from '../services/Actions.js'
 import { readFileSync } from 'fs';
+import logger from '../utils/logger.js';
 const action = new ActionsService()
 const registry_config = JSON.parse(readFileSync('./config/registry.json'))
 class CronService {
     constructor() {
        
     }
-    async sendStatusUpdatedNotification(db, recipient){
+    async sendStatusUpdatedNotification(db){
         try{
         const data = await db.get_data('orderDetails')
-        console.log('Data before getStatus--->',JSON.stringify(data))
         if(data.data.domain){
             
             for(let i=0;i<Object.keys(data.data.domain).length;i++){
                 const domainName = Object.keys(data.data.domain)[i];
-                let DOMAIN_DETAILS = {
-                    url:"",
-                    token:"",
-                }
                 switch(domainName){
                     case DOMAINS.ENERGY:
-                        DOMAIN_DETAILS = {
-                            url:ENERGY_STRAPI_URL,
-                            token:process.env.STRAPI_ENERGY_TOKEN
-                        }
                         BECKN_STATUS_CALL.context.domain=registry_config[0].bpps.energy.domain;
                         BECKN_STATUS_CALL.context.bpp_id=registry_config[0].bpps.energy.bpp_id;
                         BECKN_STATUS_CALL.context.bpp_uri=registry_config[0].bpps.energy.bpp_uri;          
                         break;
                     case DOMAINS.RETAIL:
-                        DOMAIN_DETAILS = {
-                            url:RETAIL_STRAPI_URL,
-                            token:process.env.STRAPI_RETAIL_TOKEN
-                        }
                         BECKN_STATUS_CALL.context.domain=registry_config[0].bpps.retail.domain;
                         BECKN_STATUS_CALL.context.bpp_id=registry_config[0].bpps.retail.bpp_id;
                         BECKN_STATUS_CALL.context.bpp_uri=registry_config[0].bpps.retail.bpp_uri;      
                         break;
                     case DOMAINS.HOTEL:
-                        DOMAIN_DETAILS = {
-                            url:HOTEL_STRAPI_URL,
-                            token:process.env.STRAPI_HOTEL_TOKEN
-                        }
                         BECKN_STATUS_CALL.context.domain=registry_config[0].bpps.hospitality.domain;
                         BECKN_STATUS_CALL.context.bpp_id=registry_config[0].bpps.hospitality.bpp_id;
                         BECKN_STATUS_CALL.context.bpp_uri=registry_config[0].bpps.hospitality.bpp_uri;      
                         break;
                     case DOMAINS.TOURISM:
-                        DOMAIN_DETAILS = {
-                            url:TOURISM_STRAPI_URL,
-                            token:process.env.STRAPI_TOURISM_TOKEN
-                        }
                         BECKN_STATUS_CALL.context.domain=registry_config[0].bpps.tourism.domain;
                         BECKN_STATUS_CALL.context.bpp_id=registry_config[0].bpps.tourism.bpp_id;
                         BECKN_STATUS_CALL.context.bpp_uri=registry_config[0].bpps.tourism.bpp_uri;      
@@ -90,7 +66,7 @@ class CronService {
             return {status:true, message:"Notification Sent"}
         }
         }catch(error){
-            console.log(error)
+            logger.info(`Error Occured: ${error.message}`)
             throw new Error(error.message)
         }
        
